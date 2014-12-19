ef6baac1f20fd2e17643b9d92e08ebb5b5046130
Niall Fallon <nfallon2002@gmail.com>
2014-12-19 11:25:25 +0000

    Carousel
    still needs sizing work

---
+++ scripts/mycarousel.js
@@ -0,0 +1,69 @@
+﻿/* File Created: December 19, 2014 */
+
+$(document).ready(function () {
+
+    doCarousel("main");
+    
+   
+    
+});
+ 
+function doCarousel(viewName)
+{
+    var result = new Array();
+
+    var dir = "images";
+    var fileExtension = ".jpg";
+
+    $.ajax({
+        url: dir,
+        success: function (data) {
+            $(data).find("a:contains(" + fileExtension + "),a:contains(" + fileExtension.toUpperCase() + ")").each(function () {
+
+                //sometimes need to insert the original directory name or the file will not be found!
+                var spliturl = this.href.split('/');
+                //var host = window.location.host;
+                //var websitename = spliturl[spliturl.length - 2];
+                var filename = spliturl[spliturl.length - 1];
+                //var fullPath = "http://" + host + "/" + websitename + "/" + dir + "/" + filename;
+                var fullPath = dir + "/" + filename;
+                result.push(fullPath);
+            });
+
+            drawCarousel(viewName, result);
+
+        },
+        error: function (xhr, status, errorThrown) {
+            console.log("Error: " + errorThrown);
+            console.log("Status: " + status);
+            console.dir(xhr);
+        }
+
+    });
+}
+
+function drawCarousel(viewName, arrayOfURIs)
+{
+    var indicator = $("ol.carousel-indicators");
+    var inner = $("div.carousel-inner");
+
+    //add a li for each image.
+    indicator.append("<li data-target='#myCarousel' data-slide-to='0' class='active'></li>");
+    for(var index = 1;index<= arrayOfURIs.length;index++)
+    {
+        indicator.append("<li data-target='#myCarousel' data-slide-to='"+ index +"'></li>");
+    }
+
+    var altText = "Ballyglass photo";
+    //add each image to the inner
+    inner.append("<div class='item active'><img src='" + arrayOfURIs[0] + "' alt='"+ altText +"' ></div>");
+    for(var index = 1;index<= arrayOfURIs.length;index++)
+    {
+        inner.append("<div class='item'><img src='" + arrayOfURIs[index] + "' alt='" + altText + "' ></div>");
+    }
+
+    //$.each(arrayOfURIs, function (index, value) {
+    //    console.log(value);
+    //});
+    
+}
